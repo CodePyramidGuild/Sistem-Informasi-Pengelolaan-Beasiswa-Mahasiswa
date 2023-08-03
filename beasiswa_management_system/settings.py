fd3bf38e462750e973a257af5c98dde181d3291c
adhamcahyo <nebelwerfersf@gmail.com>
2023-08-04 02:05:37 +0700

    perbaikan minor

--- beasiswa_management_system/settings.py
+++ beasiswa_management_system/settings.py
@@ -1,24 +1,16 @@
 import os
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+ 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ' '
-
-# SECURITY WARNING: don't run with debug turned on in production!
+ 
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
+ 
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
